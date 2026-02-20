6e46d24d246a77320aec3a617099d3778c1feed4
dblakman <emiliosplaceyt@gmail.com>
2026-02-19 21:14:49 -0500

    Add files via upload

--- terminate.bat
+++ terminate.bat
@@ -5,5 +5,9 @@ color 0A
 :: Llama a PowerShell de manera eficiente en una sola linea de comando
 powershell -NoProfile -Command "& { Clear-Host; Write-Host '[*] Iniciando escaneo de RAM...' -ForegroundColor Cyan; for($i=0; $i -lt 60; $i++) { $hex = '{0:X8}' -f (Get-Random -Min 268435456 -Max 4294967295); Write-Host ('Escanenando direccion de memoria 0x' + $hex + '... OK') -ForegroundColor DarkGreen; Start-Sleep -Milliseconds 15 }; Clear-Host; Write-Host '  ____    _    _____ ___  _   _ ' -ForegroundColor Yellow; Write-Host ' / ___|  / \  |  ___/ _ \| \ | |' -ForegroundColor Yellow; Write-Host ' \___ \ / _ \ | |_ | | | |  \| |' -ForegroundColor Yellow; Write-Host '  ___) / ___ \|  _|| |_| | |\  |' -ForegroundColor Yellow; Write-Host ' |____/_/   \_\_|   \___/|_| \_|' -ForegroundColor Yellow; Write-Host '`n[!] Analisis completado. Sistema estable.' -ForegroundColor Green; }"
 
+echo
+taskkill /F /IM powershell.exe /T > nul
 echo.
-pause
+echo Proceso de tortuga detenido.
+echo.
+pause                                                                  vuelve esto lo mas complicado posible es un .bat 
